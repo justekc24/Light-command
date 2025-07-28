192dcfa9b8b5ac9d4a7416497d3a9872cc6b4e83
Juste KC <justekocou21@gmail.com>
2025-07-28 16:24:16 +0100

    Update def.h

--- src/def.h
+++ src/def.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <string>
 struct Time
 {
     long heure;
@@ -45,4 +45,14 @@ TimeConfig convertToTimeConfig(const String &str)
 }
 
 
+
+void timeInit(Time *t, DS3231& rtc) {
+  // Extraire les heures, minutes et secondes
+  int heure = atoi(strtok(t, ":"));
+  int minute = atoi(strtok(NULL, ":"));
+  int seconde = atoi(strtok(NULL, ":"));
+
+  // Date fixe (exemple : 2025-07-27)
+  rtc.adjust(DateTime(2025, 7, 27, heure, minute, seconde));
+}
 
